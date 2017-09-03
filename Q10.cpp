3f6b5124dc122fbc9ffdce8e1fda19782f37d30f
kprahlad <31153973+kprahlad@users.noreply.github.com>
2017-09-03 14:08:32 +0530

    Create Q10.cpp

---
+++ Q10.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+  char a;
+  cin>>a;
+  if((a>='a'&&a<='z') cout<<"Uppper case alphabet";
+     else if(a>='A'&&a<='Z') cout<<"Lower case alphabet";
+  return 1;
+ }
