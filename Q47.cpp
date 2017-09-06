7b97984576d2392ad53bd16effdb7a830ed39b49
kprahlad <31153973+kprahlad@users.noreply.github.com>
2017-09-06 16:55:47 +0530

    Create Q47.cpp

---
+++ Q47.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  int i=1,s=0;
+  while(i<=n)
+  {
+    if(!(n%i)) s++;
+  }
+  if(s==2) cout<<"Prime";
+  return 1;
+}
