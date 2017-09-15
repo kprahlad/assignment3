5ce1eda878ae7513209aab156eb41dadbac14c83
kprahlad <31153973+kprahlad@users.noreply.github.com>
2017-09-15 19:37:00 +0530

    Update Q7.cpp

--- Q7.cpp
+++ Q7.cpp
@@ -5,7 +5,7 @@ int main()
 {
   char a;
   cin>>a;
-  if((a>='a'&&a<='z')||(a>='A'&&a<='Z') cout<<"Yup";
+  if((a>='a'&&a<='z')||(a>='A'&&a<='Z')) cout<<"Yup";
      else cout<<"No";
   return 1;
  }
